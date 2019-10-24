bdbc201c6390a6dad826d5fc265e3a69be0b9228
Hunor Debreczeni <debreczenihunor@outlook.com>
2019-10-24 14:39:43 +0300

    Heap Approaches Assignment - 02

--- Assignments/02 - Heap Stuff/main.cpp
+++ Assignments/02 - Heap Stuff/main.cpp
@@ -5,13 +5,43 @@
  *  Assignment Specification:   Implement correctly and efficiently two methods for building a heap,
  *                               namely the bottom­up and the top­down strategies.
  *                              Additionally, you have to implement heapsort.
- *                                  Implement and exemplify correctness
+ *                                  Implement and exemplify correctness - Lines 201-230
  *                                  Comparative analysis of the two build heap methods, in the average case
  *                                  Interpretations, advantages/disadvantages of each approach
  *
- *  Conclusions:
+ *      *Bottom Up*
+ *          Algorithm:
+ *              We assume that all the leaves are in the correct place, so we call the heapify only n/2 times.
+ *              On each level, the possible number of swaps increase due to the height, but the probability on which
+ *               it happens, decreases due to previous swaps.
+ *          Run Time:
+ *              Due to the fact that at the level above the leaves we can have n/4 nodes with at most 1 swap,
+ *               above n/8 nodes with at most 2 swaps..., by summing this up we can get O(n) complexity for the algorithm
+ *          Advantages:
+ *              This algorithm is the fastest one, but it's only usable if we have the elements pre-defined.
+ *              May be used as a priority queue.
+ *          Disadvantages:
+ *              We cannot use this algorithm if we don't have the elements pre-defined.
  *
- *      
+ *      *Top Down*
+ *          Algorithm:
+ *              We insert each element separately to the end of the heap and after each insert we run the heapify
+ *               algorithm to organize our heap.
+ *          Run Time:
+ *              Each insertion + heapify takes O(log n) time, and thus we have n elements, the final complexity
+ *               of the algorithm is O(n*log n)
+ *          Advantages:
+ *              We can add any time a new element and we can use it as a priority queue in which we can add new elements.
+ *          Disadvantages:
+ *              The slowest algorithm for building up a heap.
+ *
+ *      *Comparison*
+ *          In each case, the Top Down algorithm does a bit worse than the Bottom Up algorithm, but we can see that
+ *           in the best case, they only have a constant number difference between the total tasks that are done in each
+ *           algorithm.
+ *
+ *
+ *
  */
 
 #include <iostream>
@@ -61,18 +91,19 @@ void maxHeapifyBottom(int *a, int i, int n, Operation op) {
     int leftChildIndex = getLeftChildIndex(i);
     int rightChildIndex = getRightChildIndex(i);
     op.count(2);
+    op.count(2);
     if (verifyIndex(rightChildIndex, n) && (a[rightChildIndex] > a[i]) && (a[rightChildIndex] > a[leftChildIndex])) {
-        op.count(1);
         swap(a[rightChildIndex], a[i]);
         op.count(3);
         maxHeapifyBottom(a, rightChildIndex, n, op);
-    } else if (verifyIndex(leftChildIndex, n) && a[leftChildIndex] > a[i]) {
+    } else {
         op.count(1);
-        swap(a[leftChildIndex], a[i]);
-        op.count(3);
-        maxHeapifyBottom(a, leftChildIndex, n, op);
+        if (verifyIndex(leftChildIndex, n) && a[leftChildIndex] > a[i]) {
+            swap(a[leftChildIndex], a[i]);
+            op.count(3);
+            maxHeapifyBottom(a, leftChildIndex, n, op);
+        }
     }
-    //op.count(2);
 }
 
 void buildMaxHeapBottomUp(int a[], int n, Operation op) {
@@ -98,7 +129,6 @@ void maxHeapifyTop(int *array, int i, Operation op) {
         swap(array[parentIndex], array[i]);
         i = parentIndex;
         parentIndex = getParentIndex(i);
-        op.count(2);
     }
     if(parentIndex != -1){
         op.count();
@@ -106,43 +136,11 @@ void maxHeapifyTop(int *array, int i, Operation op) {
 }
 
 void buildMaxHeapTopDown(int source[], int n, Operation op) {
-    int result[n];
     for (int i = 0; i < n; i++) {
-        result[i] = source[i];
+        source[i] = source[i]; // Pretending to insert
         op.count();
-        maxHeapifyTop(result, i, op);
+        maxHeapifyTop(source, i, op);
     }
-    for (int i = 0; i < n; i++) {
-        source[i] = result[i];
-    }
-}
-
-void exemplifyCorrectness(int n) {
-    int testArray[MAX_HEAP_SIZE];
-    FillRandomArray(testArray, n, 1, 10, true, 1);
-    int a[MAX_HEAP_SIZE];
-    int b[MAX_HEAP_SIZE];
-    int c[MAX_HEAP_SIZE];
-    CopyArray(a, testArray, MAX_HEAP_SIZE);
-    CopyArray(b, testArray, MAX_HEAP_SIZE);
-    CopyArray(c, testArray, MAX_HEAP_SIZE);
-
-    Operation dummy = profiler.createOperation("Dummy", 0);
-
-    cout << "Bottom-Up Heap Creation:" << endl;
-    printArray(a, n);
-    buildMaxHeapBottomUp(a, n, dummy);
-    printArray(a, n);
-
-    cout << endl << "Heap Sort:" << endl;
-    printArray(a, n);
-    heapSort(a, n, dummy);
-    printArray(a, n);
-
-    cout << endl << "Top-Down Heap Creation:" << endl;
-    printArray(b, n);
-    buildMaxHeapTopDown(b, n, dummy);
-    printArray(b, n);
 }
 
 void worstCase(int n) {
@@ -198,6 +196,34 @@ void runTests() {
     profiler.createGroup("Worst Case", "Worst Case Bottom Up", "Worst Case Top Down");
     profiler.createGroup("Best Case", "Best Case Bottom Up", "Best Case Top Down");
     profiler.showReport();
+}
+
+void exemplifyCorrectness(int n) {
+    int testArray[MAX_HEAP_SIZE];
+    FillRandomArray(testArray, n, 1, 10, true, 1);
+    int a[MAX_HEAP_SIZE];
+    int b[MAX_HEAP_SIZE];
+    int c[MAX_HEAP_SIZE];
+    CopyArray(a, testArray, MAX_HEAP_SIZE);
+    CopyArray(b, testArray, MAX_HEAP_SIZE);
+    CopyArray(c, testArray, MAX_HEAP_SIZE);
+
+    Operation dummy = profiler.createOperation("Dummy", 0);
+
+    cout << "Bottom-Up Heap Creation:" << endl;
+    printArray(a, n);
+    buildMaxHeapBottomUp(a, n, dummy);
+    printArray(a, n);
+
+    cout << endl << "Heap Sort:" << endl;
+    printArray(a, n);
+    heapSort(a, n, dummy);
+    printArray(a, n);
+
+    cout << endl << "Top-Down Heap Creation:" << endl;
+    printArray(b, n);
+    buildMaxHeapTopDown(b, n, dummy);
+    printArray(b, n);
 }
 
 int main() {
