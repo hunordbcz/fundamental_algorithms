b153dfc9079be33922abc4b734a60dd8a4d7e23b
Hunor Debreczeni <debreczenihunor@outlook.com>
2019-11-21 16:07:28 +0200

    Minor fixes

--- 06 - Dynamic Order Statistics - BST/main.cpp
+++ 06 - Dynamic Order Statistics - BST/main.cpp
@@ -1,7 +1,38 @@
-/*
+/**
  *  Author: Hunor Debreczeni
  *
  *  Group: 30423
+ *
+ *      *Build Tree*
+ *          Logic:
+ *              Used a Divide Et Impera algorith, started from the middle, and kept going left/right.
+ *              For initializing the size of the current root, we used the size of the array, since that's the size we need.
+ *          Complexity:
+ *              Since we go through each element from 1 to n, it takes O(n) time to do it.
+ *
+ *      *OS-Select* ( Select the i-th smallest element )
+ *          Logic:
+ *              For the current order of the element, we can get the left child's size + 1, since the root has only smaller
+ *               elements on it's left and + itself makes the left child's size + 1-th smallest element.
+ *          Complexity:
+ *              Since it goes through the levels of the tree until it finds the value, it takes O(log n) time. In our
+ *               analysis, since we do it for n times, it takes O(n*log n), that's why it's slower than the build function.
+ *
+ *      *OS-Delete* ( Delete the i-th smallest element )
+ *          Logic:
+ *              For finding the element we use the same technique as the OS-Select, but while going deeper in the tree,
+ *               we decrement the current node's size, because we will remove an element under of it.
+ *              For deleting the element we use the BST Delete method with finding it's successor if needed.
+ *          Complexity:
+ *              The select method takes up O(log n) as mentioned above + the delete method also needs O(log n) because we
+ *               need to find the successor for the swap if needed.
+ *              This makes up to O(2*log n) = O(log n). In our analysis, we do it n times, so it also takes O(n*log n)
+ *
+ *      *Chart*
+ *          The fastest one is the build of the BST, because it's a linear function, takes O(n)
+ *          Afterwards comes the OS-Select method.
+ *          The OS-Delete does approximately 2 times more operations than the OS-Select, thus it has the same complexity
+ *           as the OS-Select. It's because the OS-Delete algorithm also includes the OS-Select algorithm in itself.
  *
  */
 
@@ -32,7 +63,7 @@ BTNode *buildBST(int low, int high, Operation op) {
     if (low > high) return nullptr;
     op.count();
     int mid = (low + high) / 2;
-    op.count();
+    op.count(2);
     BTNode *root = newNode(mid, high - low + 1);
     op.count();
     root->left = buildBST(low, mid - 1, op);
@@ -54,12 +85,7 @@ BTNode *osSelect(BTNode *root, int i, Operation op) {
     int currentOrder;
     op.count();
     if (root->left == nullptr) {
-        op.count();
-        if (root->right == nullptr) {
-            currentOrder = 1;
-        } else {
-            currentOrder = root->right->size;
-        }
+        currentOrder = 1;
     } else {
         currentOrder = root->left->size + 1;
     }
@@ -80,12 +106,7 @@ BTNode *osDelete(BTNode *root, int i, Operation op) {
     int currentOrder;
     op.count();
     if (root->left == nullptr) {
-        op.count();
-        if (root->right == nullptr) {
-            currentOrder = 1;
-        } else {
-            currentOrder = root->right->size;
-        }
+        currentOrder = 1;
     } else {
         currentOrder = root->left->size + 1;
     }
@@ -110,14 +131,14 @@ BTNode *osDelete(BTNode *root, int i, Operation op) {
     if (root->left == nullptr) {
         op.count();//1 or 0 nodes
         BTNode *temp = root->right;
-        free(root);
+        delete root;
         return temp;
     }
     op.count();
     if (root->right == nullptr) {    //1 or 0 nodes
         op.count();
         BTNode *temp = root->left;
-        free(root);
+        delete root;
         return temp;
     }                                //2 nodes
     op.count(2);
@@ -140,7 +161,7 @@ BTNode *osDelete(BTNode *root, int i, Operation op) {
         op.count();
         root->right = swapWith->right;
     }
-    free(swapWith);
+    delete swapWith;
     return root;
 }
 
@@ -166,22 +187,27 @@ void print(BTNode *root) {
 
 void runTests() {
     for (int n = 100; n <= 10000; n += 100) {
+        Operation buildOp = profiler.createOperation("Build", n);
+        Operation selectOp = profiler.createOperation("Select", n);
+        Operation deleteOp = profiler.createOperation("Delete", n);
+        Operation totalOp = profiler.createOperation("Total", n);
         for (int repeat = 0; repeat < 5; repeat++) {
-            Operation buildOp = profiler.createOperation("Build", n);
-            Operation selectOp = profiler.createOperation("Select", n);
-            Operation deleteOp = profiler.createOperation("Delete", n);
+
             BTNode *temp = buildTree(n, buildOp);
+            int nTemp = n + 1;
             for (int i = 0; i < n; i++) {
-                int index = rand() % n + 1;
+                int index = rand() % nTemp-- + 1;
                 osSelect(temp, index, selectOp);
                 osDelete(temp, index, deleteOp);
             }
         }
+        totalOp.count(buildOp.get() + selectOp.get() + deleteOp.get());
     }
     profiler.divideValues("Build", 5);
     profiler.divideValues("Select", 5);
     profiler.divideValues("Delete", 5);
-    profiler.createGroup("Group", "Build", "Select", "Delete");
+    profiler.divideValues("Total", 5);
+    profiler.createGroup("Group", "Build", "Select", "Delete", "Total");
     profiler.showReport();
 }
 
