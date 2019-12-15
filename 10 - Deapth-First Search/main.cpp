888085c91a7ec372d9c50aa5d88ad66b631e863c
Hunor Debreczeni <debreczenihunor@outlook.com>
2019-12-15 22:35:14 +0200

    Tarjan's Algorithm implemented

--- 10 - Deapth-First Search/main.cpp
+++ 10 - Deapth-First Search/main.cpp
@@ -12,36 +12,114 @@ enum {
 
 typedef struct _Node {
     int key;
-    vector<_Node*> adj;
+    set<_Node *> adj;
     int color;
+
+    bool onStack = false;
+    int disc = -1;
+    int low = -1;
 } Node;
 
-void dfs(Node *root){
+Node *newNode(int key) {
+    auto *node = new Node;
 
+    node->key = key;
+    node->color = COLOR_WHITE;
+    return node;
 }
 
-vector<Node*> generateConnectedGraph(int nrNodes, int nrEdges) {
+vector<Node *> generateConnectedGraph(int nrNodes, int nrEdges) {
     int i;
-    vector<set < int>> adj(nrNodes);
-    vector<Node*> graph(nrNodes);
-    for (i = 1; i < nrNodes; i++) {
-        int a = rand() % i;
-        graph[i]->key=i;
-        graph[i]->adj.push_back(graph[a]);
-//        adj[a].insert(a);
+    vector<Node *> graph;
+    for (i = 0; i < nrNodes; i++) {
+        graph.push_back(newNode(i));
     }
-    while (i <= nrEdges) {
+    i = 0;
+    while (i < nrEdges) {
         int a = rand() % nrNodes, b = rand() % nrNodes;
-        if (a != b && !adj[a].count(b) && !adj[b].count(a)) {
-            graph[a]->adj.push_back(graph[b]);
+        if (a != b && !graph[a]->adj.count(graph[b])) {
+            graph[a]->adj.insert(graph[b]);
             i++;
         }
     }
-
     return graph;
 }
 
+void dfs(Node *root) {
+    root->color = COLOR_BLACK;
+    cout << root->key << endl;
+    set<Node *>::iterator i;
+    for (i = root->adj.begin(); i != root->adj.end(); ++i) {
+        Node *element = *i;
+        if (element->color != COLOR_BLACK) {
+            dfs(element);
+        }
+    }
+}
+
+void tarjan(Node *u, vector<Node *> &stack, int &index) {
+
+    u->disc = u->low = ++index;
+    stack.push_back(u);
+    u->onStack = true;
+
+    set<Node *>::iterator i;
+    for (i = u->adj.begin(); i != u->adj.end(); i++) {
+        Node *element = *i;
+        if (element->disc == -1) {
+            tarjan(element, stack, index);
+            u->low = min(u->low, element->low);
+        } else if (element->onStack) {
+            u->low = min(u->low, element->disc);
+        }
+    }
+
+    Node *popped;
+    if(u->low == u->disc){
+        while(stack.back()!= u){
+            popped = stack.back();
+            cout<<popped->key<< " ";
+            popped->onStack = false;
+            stack.pop_back();
+        }
+        popped = stack.back();
+        cout<<popped->key<<endl;
+        popped->onStack=false;
+        stack.pop_back();
+    }
+
+}
+
+void scc(vector<Node *> graph) {
+    vector<Node *> stack;
+    int index = 0;
+    for (auto & i : graph) {
+        if (i->disc == -1) {
+            tarjan(i, stack, index);
+        }
+    }
+}
+
+void printGraphAdj(vector<Node*> graph){
+    cout<<"Graph with adjacency nodes:\n";
+    for(int i =0;i<graph.size();i++){
+        cout<<i<<": ";
+        set<Node *>::iterator j;
+        for (j = graph[i]->adj.begin(); j != graph[i]->adj.end(); j++) {
+            Node *element = *j;
+            cout<<element->key<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 int main() {
-    int a = 10;
-    vector<Node*> graph = generateConnectedGraph(10, 30);
+    srand(time(nullptr));
+    vector<Node *> graph = generateConnectedGraph(5, 8);
+    printGraphAdj(graph);
+    cout<<"\nDepth-First Search:\n";
+    dfs(graph[0]);
+    cout<<"\nSCC\n";
+    scc(graph);
+    return 0;
 }
