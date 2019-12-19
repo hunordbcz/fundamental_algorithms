72a74e2a19516f0a6135b9229dfd72fac19527f3
Hunor Debreczeni <debreczenihunor@outlook.com>
2019-12-19 03:08:50 +0200

    Done

--- 10 - Deapth-First Search/main.cpp
+++ 10 - Deapth-First Search/main.cpp
@@ -1,19 +1,63 @@
+/**
+ *  Author: Hunor Debreczeni
+ *  Group: 30423
+ *
+ *  Assignment: Implement correctly and efficiently:
+ *                  - the depth­first search algorithm
+ *                  - Tarjan algorithm for detecting strongly connected components
+ *                  - topological sorting
+ *
+ *      *Depth-First Search*:
+ *          Algorithm: Goes deep through the adjacent nodes, then take the other children
+ *          Complexity:
+ *              It depends mostly on the edges, because the node from where we start may have no adjacent nodes.
+ *              This way, the algorithm will have a complexity of O(1).
+ *              Otherwise, if we get 'lucky', the complexity depends both on the vertexes and the edges,
+ *               so it will be O(V + E).
+ *
+ *      *Tarjan Algorithm*:
+ *          Logic:
+ *              Finds all the strongly connected graphs ( there is a path between each node ).
+ *              The program indexes each node with the smallest node that it can reach. If we finish marking the
+ *               nodes and can't go further, we print the nodes from the stack until we reach the original node.
+ *          Complexity:
+ *              Goes through each edge for all the vertexes, so it has an O( V + E ) complexity.
+ *
+ *      *Topological Sort*:
+ *          Logic:
+ *              Same as DFS with the only difference that it goes through each node and we save the path of the tree
+ *               in a stack, which will be the result of the topological sort at the end.
+ *              In case the graph has a loop, it can't be sorted topologically. For this check I used Tarjan's algorithm,
+ *               to check if there's a loop or not. In this case, the result will be an empty list.
+ *          Complexity:
+ *              Because it's a Directed Acyclic Graph, we don't have a problem with not having adjacent nodes,
+ *               so it's of O( V + E ).
+ *
+ *      *Chart*:
+ *          On both cases, with random edges and with random vertexes, we can clearly see, that the running time is
+ *           of O( V + E ). For each vertex we set it's color and for each edge we check if the other node was colored
+ *           already or not. We have many edges and many vertexes so our chance for the node to have zero adjacent nodes
+ *           is very small, and so on for the next one, and so on.
+ *
+ */
+
 #include <iostream>
 #include <set>
 #include "Profiler.h"
 
 using namespace std;
 
+Profiler profiler("Depth-First Search");
+
 enum {
     COLOR_WHITE = 0,
-    COLOR_GRAY,
     COLOR_BLACK
 };
 
 typedef struct _Node {
-    int key;
+    int key{};
     set<_Node *> adj;
-    int color;
+    int color{};
 
     bool onStack = false;
     int disc = -1;
@@ -45,19 +89,21 @@ vector<Node *> generateConnectedGraph(int nrNodes, int nrEdges) {
     return graph;
 }
 
-void dfs(Node *root) {
+void dfs(Node *root, Operation op) {
+    op.count();
     root->color = COLOR_BLACK;
     cout << root->key << endl;
     set<Node *>::iterator i;
     for (i = root->adj.begin(); i != root->adj.end(); ++i) {
         Node *element = *i;
+        op.count();
         if (element->color != COLOR_BLACK) {
-            dfs(element);
+            dfs(element, op);
         }
     }
 }
 
-void tarjan(Node *u, vector<Node *> &stack, int &index) {
+void tarjanRec(Node *u, vector<Node *> &stack, int &index, bool &hasLoop, bool print) {
 
     u->disc = u->low = ++index;
     stack.push_back(u);
@@ -67,7 +113,7 @@ void tarjan(Node *u, vector<Node *> &stack, int &index) {
     for (i = u->adj.begin(); i != u->adj.end(); i++) {
         Node *element = *i;
         if (element->disc == -1) {
-            tarjan(element, stack, index);
+            tarjanRec(element, stack, index, hasLoop, print);
             u->low = min(u->low, element->low);
         } else if (element->onStack) {
             u->low = min(u->low, element->disc);
@@ -75,51 +121,143 @@ void tarjan(Node *u, vector<Node *> &stack, int &index) {
     }
 
     Node *popped;
-    if(u->low == u->disc){
-        while(stack.back()!= u){
+    if (u->low == u->disc) {
+        while (stack.back() != u) {
+            hasLoop = true;
             popped = stack.back();
-            cout<<popped->key<< " ";
+            if(print) {
+                cout << popped->key << " ";
+            }
             popped->onStack = false;
             stack.pop_back();
         }
         popped = stack.back();
-        cout<<popped->key<<endl;
-        popped->onStack=false;
+        if(print) {
+            cout << popped->key << endl;
+        }
+        popped->onStack = false;
         stack.pop_back();
     }
-
 }
 
-void scc(vector<Node *> graph) {
+bool tarjan(vector<Node *> graph, bool print) {
+    bool hasLoop = false;
     vector<Node *> stack;
     int index = 0;
-    for (auto & i : graph) {
+    for (auto &i : graph) {
         if (i->disc == -1) {
-            tarjan(i, stack, index);
+            tarjanRec(i, stack, index, hasLoop, print);
         }
     }
+    return hasLoop;
 }
 
-void printGraphAdj(vector<Node*> graph){
-    cout<<"Graph with adjacency nodes:\n";
-    for(int i =0;i<graph.size();i++){
-        cout<<i<<": ";
+void printGraphAdj(vector<Node *> graph) {
+    cout << "Graph with adjacency nodes:\n";
+    for (int i = 0; i < graph.size(); i++) {
+        cout << i << ": ";
         set<Node *>::iterator j;
         for (j = graph[i]->adj.begin(); j != graph[i]->adj.end(); j++) {
             Node *element = *j;
-            cout<<element->key<<" ";
+            cout << element->key << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
+}
+
+void topologicalSortRec(Node *node, vector<Node *> &stack) {
+    node->color = COLOR_BLACK;
+    set<Node *>::iterator j;
+    for (j = node->adj.begin(); j != node->adj.end(); j++) {
+        Node *element = *j;
+        if (element->color != COLOR_BLACK) {
+            topologicalSortRec(element, stack);
+        }
+    }
+    stack.push_back(node);
+}
+
+void topologicalSort(vector<Node *> graph) {
+    cout<<"\nTopological sort:\n";
+    if(tarjan(graph,false)){
+        cout<<"\nLoop inside, graph -> it's not a DAG ( Can't run Topological Sort )\n";
+        return;
+    }
+    vector<Node *> stack;
+    for (auto &i : graph) {
+        if (i->color != COLOR_BLACK) {
+            topologicalSortRec(i, stack);
+        }
+    }
+
+    while (!stack.empty()){
+        cout<<stack.back()->key<<" ";
+        stack.pop_back();
+    }
+    cout<<endl;
+}
+
+void runTests() {
+    for (int i = 1000; i <= 5000; i += 100) {
+        Operation op = profiler.createOperation("Fixed Nodes - 100", i);
+        cout << i << endl;
+        vector<Node *> graph = generateConnectedGraph(100, i);
+        dfs(graph[rand() % 100], op);
+    }
+    for (int i = 100; i <= 200; i += 10) {
+        Operation op = profiler.createOperation("Fixed Edges - 9000", i);
+        cout << i << endl;
+        vector<Node *> graph = generateConnectedGraph(i, 9000);
+        dfs(graph[rand() % i], op);
+    }
+    profiler.showReport();
+}
+
+vector<Node *> getDAG() {
+    int i;
+    vector<Node *> graph;
+    for (i = 0; i < 8; i++) {
+        graph.push_back(newNode(i));
+    }
+    graph[0]->adj.insert(graph[1]);
+    graph[0]->adj.insert(graph[2]);
+    graph[1]->adj.insert(graph[2]);
+    graph[1]->adj.insert(graph[3]);
+    graph[2]->adj.insert(graph[3]);
+    graph[2]->adj.insert(graph[5]);
+    graph[3]->adj.insert(graph[4]);
+    graph[7]->adj.insert(graph[6]);
+
+    return graph;
+}
+
+void demo() {
+    cout << "\nDemo:\n";
+    Operation dummy = profiler.createOperation("dummy", 0);
+    vector<Node *> graph = generateConnectedGraph(5, 8);
+    vector<Node *> graphRand = generateConnectedGraph(5, 8);
+    vector<Node *> graphDAG = getDAG();
+
+    printGraphAdj(graph);
+
+    cout << "\nDepth-First Search from node 0:\n";
+    dfs(graph[0], dummy);
+
+    cout << "\nStrongly Connected Components:\n";
+    tarjan(graph,true);
+
+    cout<<"\nTopological Sort with a random graph:\n";
+    printGraphAdj(graphDAG);
+    topologicalSort(graphRand);
+
+    cout<<"\nLet's try with a DAG now\n";
+    printGraphAdj(graphDAG);
+    topologicalSort(graphDAG);
 }
 
 int main() {
     srand(time(nullptr));
-    vector<Node *> graph = generateConnectedGraph(5, 8);
-    printGraphAdj(graph);
-    cout<<"\nDepth-First Search:\n";
-    dfs(graph[0]);
-    cout<<"\nSCC\n";
-    scc(graph);
+    runTests();
+    demo();
     return 0;
 }
